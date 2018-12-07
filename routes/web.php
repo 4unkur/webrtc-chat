b5ddf0f1a7bca54a637ff059e3389ef4f8f61071
Daiyrbek Artelov <4unkur@mail.ru>
2018-12-07 19:52:23 +0600

    Default redirect path set to /

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 /** @var Illuminate\Routing\Router $router */
 
-$router->get('', 'ChatController@index');
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+$router->get('', 'ChatController@index');
